6c4f3e28890b13175e0a6b441f66a78f30c85616
sbordelais <30980111+sbordelais@users.noreply.github.com>
2017-09-07 16:18:33 +0200

    Use 'path' module

--- citizeng-demo.js
+++ citizeng-demo.js
@@ -1,22 +1,23 @@
 // Requirements.
-var citizeng = require("./citizeng");
+var citizeng  = require('./citizeng');
+var path      = require('path');
 
 // Initialize the server.
 citizeng.init(3030, "root", "root", __dirname);
 
 // Declare main page route.
 citizeng.get  ( "/"
-              , (__dirname + "/index-demo.html")
-              , (__dirname + "/indexsuper-demo.html"));
+              , path.join(__dirname, "index-demo.html")
+              , path.join(__dirname, "indexsuper-demo.html"));
 
 // Declare other page route.
 citizeng.get  ( "/other"
-               , (__dirname + "/other-demo.html"));
+              , path.join(__dirname, "other-demo.html"));
 
 // Declare other page route.
 citizeng.get  ( "/othersuper"
               , null
-              , (__dirname + "/othersuper-demo.html"));
+              , path.join(__dirname, "othersuper-demo.html"));
 
 // Declare a reserved page route.
 citizeng.get("/admin", null, null);
