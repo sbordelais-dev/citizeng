f6b845ecf9e0b7d8567baa6044f70a691f81f0be
sbordelais <30980111+sbordelais@users.noreply.github.com>
2017-10-15 23:59:06 +0200

    Detect client information

--- js/common.js
+++ js/common.js
@@ -75,3 +75,66 @@ function httpGet(url, callback)
   xmlHttp.send(null);
 }
 
+/* OS type */
+function ctzGetOS() {
+  var userAgent = window.navigator.userAgent,
+  platform = window.navigator.platform,
+  macosPlatforms = ["Macintosh", "MacIntel", "MacPPC", "Mac68K"],
+  windowsPlatforms = ["Win32', "Win64", "Windows", "WinCE"],
+  iosPlatforms = ["iPhone", "iPad", "iPod"],
+  os = "unknown";
+
+  if (macosPlatforms.indexOf(platform) !== -1) {
+    os = "Mac OS";
+  } else if (iosPlatforms.indexOf(platform) !== -1) {
+    os = "iOS";
+  } else if (windowsPlatforms.indexOf(platform) !== -1) {
+    os = "Windows";
+  } else if (/Android/.test(userAgent)) {
+    os = "Android";
+  } else if (!os && /Linux/.test(platform)) {
+    os = "Linux";
+  }
+
+  // Done.
+  return os;
+}
+
+/* Brower version */
+function ctzGetBrowser() {
+  var isIE = false;
+  var isChrome = false;
+  var isOpera = false;
+
+  // Opera 8.0+.
+  if ((isOpera = ((!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0))) {
+    return "opera";
+  }
+  // Firefox 1.0+.
+  else if (typeof InstallTrigger !== 'undefined') {
+    return "firefox";
+  }
+  // Safari 3.0+ "[object HTMLElementConstructor]".
+  else if (/constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || safari.pushNotification)) {
+    return "safari";
+  }
+  // Internet Explorer 6-11
+  else if (/*@cc_on!@*/false || !!document.documentMode) {
+    return "ie";
+  }
+  // Edge 20+.
+  else if ((isIE = (!isIE && !!window.StyleMedia))) {
+    return "edge";
+  }
+  // Chrome 1+.
+  else if ((isChrome = (!!window.chrome && !!window.chrome.webstore))) {
+    return "chrome";
+  }
+  // Blink engine detection.
+  else if ((isChrome || isOpera) && !!window.CSS) {
+    return "blink";
+  }
+  else {
+    return "unknown";
+  }
+}
